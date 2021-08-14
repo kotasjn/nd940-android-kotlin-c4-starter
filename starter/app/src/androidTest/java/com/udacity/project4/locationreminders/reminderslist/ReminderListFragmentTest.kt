6ad403b3516986050b3af22b72f2ea33edf0b568
Jan Kotas <kotasjn@gmail.com>
2021-08-14 21:53:12 +0200

    Add tests for ReminderListFragment

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,21 +1,38 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
+import android.app.Application
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorFragment
+import com.udacity.project4.utils.EspressoIdlingResource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
@@ -23,9 +40,128 @@ import org.mockito.Mockito.verify
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
-class ReminderListFragmentTest {
+class ReminderListFragmentTest : AutoCloseKoinTest() {
 
-//    TODO: test the navigation of the fragments.
-//    TODO: test the displayed data on the UI.
-//    TODO: add testing for the error messages.
+    private lateinit var repository: ReminderDataSource
+
+    private lateinit var appContext: Application
+
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    private lateinit var reminder: ReminderDTO
+
+    private lateinit var reminderList: MutableList<ReminderDTO>
+
+    @Before
+    fun init() {
+        stopKoin()//stop the original app koin
+        appContext = getApplicationContext()
+        val myModule = module {
+            viewModel {
+                RemindersListViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { LocalDB.createRemindersDao(appContext) }
+        }
+        //declare a new koin module
+        startKoin {
+            modules(listOf(myModule))
+        }
+        //Get our real repository
+        repository = get()
+
+        //clear the data to start fresh
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+    }
+
+    @Before
+    fun initTestVariables() {
+        reminder = ReminderDTO(
+            "TEST_TITLE",
+            "TEST_DESCRIPTION",
+            "TEST_LOCATION",
+            50.0,
+            15.0
+        )
+
+        reminderList = mutableListOf(
+            reminder,
+            ReminderDTO(
+                "TEST_TITLE2",
+                "TEST_DESCRIPTION2",
+                "TEST_LOCATION2",
+                60.0,
+                25.0
+            ),
+            ReminderDTO(
+                "TEST_TITLE3",
+                "TEST_DESCRIPTION3",
+                "TEST_LOCATION3",
+                70.0,
+                35.0
+            )
+        )
+    }
+
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    @Test
+    fun clickAddReminderFabButton_navigateToSaveReminderFragment() {
+        // GIVEN - On the list screen
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navigationMock = mock(NavController::class.java)
+        dataBindingIdlingResource.monitorFragment(scenario)
+
+        scenario.onFragment { fragment ->
+            Navigation.setViewNavController(fragment.view!!, navigationMock)
+        }
+
+        // WHEN - Click on the fab button
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        // THEN - navigate to fragment
+        verify(navigationMock).navigate(ReminderListFragmentDirections.toSaveReminder())
+    }
+
+    @Test
+    fun checkWhetherNoDataIsShown() {
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        dataBindingIdlingResource.monitorFragment(scenario)
+        onView(withId(R.id.noDataTextView)).check(matches(withText(appContext.getString(R.string.no_data))))
+    }
+
+    @Test
+    fun checkIfReminderListIsDisplayed() {
+        // GIVEN - save a reminder
+        runBlocking {
+            reminderList.forEach {
+                repository.saveReminder(it)
+            }
+        }
+
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        dataBindingIdlingResource.monitorFragment(scenario)
+
+        // THEN - List data are displayed
+        onView(withId(R.id.reminderssRecyclerView)).check(matches(isDisplayed()))
+        onView(withText("TEST_TITLE")).check(matches(isDisplayed()))
+        onView(withText("TEST_TITLE2")).check(matches(isDisplayed()))
+        onView(withText("TEST_TITLE3")).check(matches(isDisplayed()))
+    }
+
 }
