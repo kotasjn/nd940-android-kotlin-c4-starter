f6a837fe67ccf52cd7dd0bc01eb2e3293696576a
Jan Kotas <kotasjn@gmail.com>
2021-08-15 00:24:24 +0200

    Add FakeDataSource implementation, add util classes and RemindersListViewModel tests, update some gradle dependencies

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,25 +4,34 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
-
-//    TODO: Create a fake data source to act as a double to the real data source
+class FakeDataSource(
+    var reminders: MutableList<ReminderDTO>? = mutableListOf(),
+    var returnError: Boolean = false
+) :
+    ReminderDataSource {
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (returnError) return Result.Error("Test error")
+        reminders?.let { return Result.Success(ArrayList(it)) }
+        return Result.Error("Reminders not found")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (returnError) return Result.Error("Test error")
+        reminders?.let { reminders ->
+            reminders.forEach {
+                if (it.id == id) return Result.Success(it)
+            }
+        }
+        return Result.Error("Reminders not found")
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
-
 
 }
--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,103 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.app.Application
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    private lateinit var dataSource: FakeDataSource
+
+    private lateinit var viewModel: RemindersListViewModel
+
+    private lateinit var context: Application
+
+    private lateinit var reminderList: MutableList<ReminderDTO>
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Before
+    fun init() {
+        stopKoin()
+        reminderList = mutableListOf(
+            ReminderDTO(
+                "TEST_TITLE1",
+                "TEST_DESCRIPTION1",
+                "TEST_LOCATION1",
+                60.0,
+                25.0
+            ),
+            ReminderDTO(
+                "TEST_TITLE2",
+                "TEST_DESCRIPTION2",
+                "TEST_LOCATION2",
+                70.0,
+                35.0
+            )
+        )
+        context = ApplicationProvider.getApplicationContext()
+        dataSource = FakeDataSource(reminderList)
+        viewModel = RemindersListViewModel(context, dataSource)
+    }
+
+    @Test
+    fun loadRemindersNotEmpty() {
+        viewModel.loadReminders()
+        val remindersList = viewModel.remindersList.getOrAwaitValue()
+        Assert.assertThat(remindersList.isNotEmpty(), Matchers.`is`(true))
+    }
+
+    @Test
+    fun showNoDataIsInvalid() {
+        viewModel.loadReminders()
+        val showNoData = viewModel.showNoData.getOrAwaitValue()
+        Assert.assertThat(showNoData, Matchers.`is`(false))
+    }
+
+    @Test
+    fun loadRemindersExpectSuccess() = runBlockingTest {
+        dataSource.returnError = false
+        viewModel.loadReminders()
+        MatcherAssert.assertThat(
+            viewModel.showNoData.getOrAwaitValue(),
+            CoreMatchers.`is`(false)
+        )
+    }
+
+    @Test
+    fun loadRemindersExpectError() = runBlockingTest {
+        dataSource.returnError = true
+        viewModel.loadReminders()
+        MatcherAssert.assertThat(
+            viewModel.showNoData.getOrAwaitValue(),
+            CoreMatchers.`is`(true)
+        )
+    }
 
 }
